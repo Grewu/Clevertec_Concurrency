491269a6cc979ed15b0bc34068021082905291cd
Grewu <pavelgurevich97@gmail.com>
2023-12-06 09:44:58 +0300

    fix code

--- src/main/java/org/example/entity/Client.java
+++ src/main/java/org/example/entity/Client.java
@@ -1,18 +1,27 @@
 package org.example.entity;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 
 
-public class Client implements Runnable {
+class Client implements Runnable {
     private final List<Integer> dataList;
     private final Server server;
+    private final ExecutorService executorService;
+    private final List<CompletableFuture<Void>> futures;
 
     public Client(List<Integer> dataList, Server server) {
         this.dataList = dataList;
         this.server = server;
+        this.executorService = Executors.newFixedThreadPool(dataList.size());
+        this.futures = new ArrayList<>();
     }
 
     @Override
@@ -21,14 +30,30 @@ public class Client implements Runnable {
             int randomIndex = ThreadLocalRandom.current().nextInt(dataList.size());
             int value = dataList.remove(randomIndex);
 
-            CompletableFuture.runAsync(() -> {
+            Request request = new Request(value);
+
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
                 try {
-                    Thread.sleep(ThreadLocalRandom.current().nextInt(401) + 100);
-                    server.processRequest(value);
+                    Thread.sleep(new Random().nextInt(401) + 100);
+                    server.processRequest(request);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            });
+            }, executorService);
+
+            futures.add(future);
         }
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+
+        try {
+            allOf.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
     }
+
 }
+
